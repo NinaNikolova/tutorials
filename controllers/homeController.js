fce2c2ec6b451224bd4faa31638cb18bdead07c7
nina <ninagbs@abv.bg>
2023-06-19 15:35:45 +0300

    user-home view and logic

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,15 +1,21 @@
+const { getAllByDate } = require('../services/courseService');
+
 const homeController = require('express').Router();
 
-homeController.get('/', (req, res) => {
+homeController.get('/', async (req, res) => {
+    let view;
+    let courses = [];
     if (req.user) {
+        courses = await getAllByDate()
         res.render('user-home', {
             title: 'Home Page',
-
+            courses
         })
+
     } else {
         res.render('guest-home', {
             title: 'Home Page',
-
+            courses
         })
     }
 
