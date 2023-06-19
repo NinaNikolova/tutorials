1f320c3e13d207b4b9d436a2e1ec3e8bf5b23038
nina <ninagbs@abv.bg>
2023-06-19 13:29:16 +0300

    logic for user-home and guest-home views

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,18 @@
 const homeController = require('express').Router();
 
-homeController.get('/', (req,res)=>{
-    res.render('home', {
-        title: 'Home Page',
-        user: req.user
-    })
+homeController.get('/', (req, res) => {
+    if (req.user) {
+        res.render('user-home', {
+            title: 'Home Page',
+
+        })
+    } else {
+        res.render('guest-home', {
+            title: 'Home Page',
+
+        })
+    }
+
 })
 
 module.exports = homeController;
