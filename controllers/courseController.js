89bfa1a6517a4562aec135ca20100b6308481b6e
nina <ninagbs@abv.bg>
2023-06-19 14:27:36 +0300

    added create - view and action

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,11 +1,39 @@
+const { create, deleteById, getAllByDate, getById, edit } = require('../services/courseService');
+const { parseError } = require('../util/parser');
+
+
 const courseController = require('express').Router();
 
 // TODO replace with real controller by assignment
 courseController.get('/create', (req, res) => {
     res.render('create', {
+        title: 'Create page',
         user: req.user
     })
 })
+courseController.post('/create', async (req, res) => {
+    // In an Express.js application, req.body is a property of the request object (req). It is used to access the data sent in the body of an HTTP POST, PUT, or PATCH request.
+    const course = {
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        duration: req.body.duration,
+        owner: req.user._id
+    }
+    try {
+        await create(course);
+        res.redirect('/')
+    } catch (error) {
+        const errors = parseError(error)
+        res.render('create', {
+            title: 'Create Page',
+            errors,
+            body: course
+        })
+    }
+
+})
+
 courseController.get('/:id/details', (req, res) => {
     res.render('details', {
         user: req.user
@@ -17,7 +45,7 @@ courseController.get('/:id/edit', (req, res) => {
     })
 })
 courseController.get('/:id/delete', (req, res) => {
-   
+
 })
 
 module.exports = courseController;
